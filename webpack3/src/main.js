750f715043b942267fc5abb84b7b4974cef9eabe
8bulbs <286630571@qq.com>
2018-05-15 09:21:14 +0800

    init

--- webpack3/src/main.js
+++ webpack3/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import iView from 'iview'
 // import 'iview/dist/styles/iview.css'
+// 引入全局样式
 import '@/assets/styles/index.styl'
 import store from './store'
 import axios from 'axios'
