602471240ecabb0151aa057dc1e7266e83945e03
8bulbs <286630571@qq.com>
2018-05-29 19:12:20 +0800

    init

---
+++ webpack4/src/assets/utils/util.js
@@ -0,0 +1,12 @@
+const util = {}
+
+util.GetQueryString = () => {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
+  var r = window.location.search.substr(1).match(reg)
+  if (r != null) return unescape(r[2])
+  return null
+}
+util.getUrlData = (url) => {
+  return url.slice((url.indexOf('?') + 1), url.length)
+}
+export default util
--- webpack4/src/main.js
+++ webpack4/src/main.js
@@ -6,11 +6,17 @@ import iView from 'iview'
 import '@/assets/styles/index.styl'
 import store from './store'
 import axios from 'axios'
+import getData from '@/libs/getData'
 
 Vue.use(iView)
 
-Vue.prototype.$ = axios
+Vue.prototype.$ = getData
 // axios.defaults.baseURL = "/api/"
+if (process.env.NODE_ENV !== 'production') {
+  axios.defaults.baseURL = '/api/'
+} else {
+  axios.defaults.baseURL = '/'
+}
 
 new Vue({
   el: '#app',
